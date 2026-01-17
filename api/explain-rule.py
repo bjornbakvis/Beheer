8aaaa884525880ea3cb260a355a9d8512fc4c9e7
bjornbakvis <bjornbakvis@gmail.com>
2026-01-17 08:32:14 +0100

    Update explain-rule.py

--- api/explain-rule.py
+++ api/explain-rule.py
@@ -36,7 +36,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_header("Content-Type", "application/json")
         self.send_header("Content-Length", str(len(body)))
         self.send_header("Cache-Control", "no-store")
-        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(body)
 

d14eea30e74d99aa64fadc2f4dca86aaa3c1be91
bjornbakvis <bjornbakvis@gmail.com>
2026-01-18 08:16:06 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Overview from './Overview';
 import Dynamiekregels from './Dynamiekregels';
 import DynamiekregelDetail from './DynamiekregelDetail';
 import RuleDetail from './RuleDetail';
@@ -16,12 +17,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthGate>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Overview />} />
+          <Route path="/acceptatieregels" element={<App />} />
+
           <Route path="/dynamiekregels" element={<Dynamiekregels />} />
           <Route path="/dynamiekregels/:regelId" element={<DynamiekregelDetail />} />
+
           <Route path="/producten" element={<Products />} />
           <Route path="/producten/:productId/regels" element={<ProductRules />} />
           <Route path="/producten/:productId/dynamiekregels" element={<ProductDynamiekregels />} />
+
           <Route path="/rules/:regelId" element={<RuleDetail />} />
         </Routes>
       </AuthGate>
